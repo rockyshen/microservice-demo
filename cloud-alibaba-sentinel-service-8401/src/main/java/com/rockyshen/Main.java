2e388e888a3f645c31c6e3c23daf8c79de2ec74b
rockyshen <875430544@qq.com>
2025-12-08 13:37:52 +0800

    feat: 1、新增module：sentinel实验工程；2、优化父子pom，才用dependencyManagement的方式

---
+++ cloud-alibaba-sentinel-service-8401/src/main/java/com/rockyshen/Main.java
@@ -0,0 +1,11 @@
+package com.rockyshen;
+
+/**
+ * @author rockyshen
+ * @date 2025/12/8 11:41
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
