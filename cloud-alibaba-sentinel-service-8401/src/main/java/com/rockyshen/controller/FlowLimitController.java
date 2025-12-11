61fc6d22ab9fa2b75092f68bee0eb56aab51df7b
rockyshen <875430544@qq.com>
2025-12-11 10:08:44 +0800

    feat: 完成熔断策略：慢比例调用的学习

--- cloud-alibaba-sentinel-service-8401/src/main/java/com/rockyshen/controller/FlowLimitController.java
+++ cloud-alibaba-sentinel-service-8401/src/main/java/com/rockyshen/controller/FlowLimitController.java
@@ -3,6 +3,8 @@ package com.rockyshen.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * 流量监控
  * @author rockyshen
@@ -24,5 +26,22 @@ public class FlowLimitController {
     public String testC(){
         System.out.println(System.currentTimeMillis() + "-----testC 排队等待");
         return "-----testC";
+    }
+
+    /**
+     * 利用jmeter：10个线程，1秒钟全部发完。
+     * 这里的服务端响应时间设置了1秒。每个请求一定会大于熔断规则设置的200ms，符合熔断条件
+     * 所以当线程开启1秒后，就会进入熔断状态
+     * @return
+     */
+    @GetMapping("/testD")
+    public String testD() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        System.out.println("-----测试：新增熔断规则-慢调用比例");
+        return "testF 洗增熔断规则-慢调用比例";
     }
 }
