3df91166cb9b70a43b65f7f1af7ab619ce225ae6
rockyshen <875430544@qq.com>
2025-12-16 10:29:02 +0800

    feat: 新增Gateway模块，将sentinel流控机制整合到gateway中。

---
+++ cloud-alibaba-gateway-9528/src/main/java/com/rockyshen/Gateway9528.java
@@ -0,0 +1,17 @@
+package com.rockyshen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author rockyshen
+ * @date 2025/12/16 09:49
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Gateway9528 {
+    public static void main(String[] args) {
+        SpringApplication.run(Gateway9528.class);
+    }
+}
